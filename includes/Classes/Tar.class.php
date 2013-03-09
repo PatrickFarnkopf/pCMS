bc44992357fcf1ddc9e3e65b25beb6b5114ff4cc
Patrick Farnkopf <patrickfarnkopf@web.de>
2013-03-09 08:06:09 +0100

    Classes/Tar: Entferne Tabs

--- includes/Classes/Tar.class.php
+++ includes/Classes/Tar.class.php
@@ -13,43 +13,43 @@
 namespace Classes;
 
 class Tar {
-	const TYPE_DIRECTORY = 5;
+    const TYPE_DIRECTORY = 5;
 
-	private $file, $header, $content;
+    private $file, $header, $content;
     public function __construct($filename) {
-    	if (!file_exists($filename))
-    		throw new \Exception("Can not open file $filename. File does not exist!", 0x0A);
-    	
-    	$this->file = fopen($filename, 'a+');
+        if (!file_exists($filename))
+            throw new \Exception("Can not open file $filename. File does not exist!", 0x0A);
+        
+        $this->file = fopen($filename, 'a+');
         $this->content = fread($this->file, filesize($filename));
         $this->header = $this->readHeader();
     }
 
     private function readHeader() {
-    	$header = [];
-		while (strlen($bin = fread($this->file, 512)) !== 0) {
-			$this->headerBin[] = $bin;
-			$data = unpack("a100filename/a8mode/a8uid/a8gid/a12size/a12mtime/a8checksum/a1typeflag/a100link/a6magic/a2version/a32uname/a32gname/a8devmajor/a8devminor/a155prefix", $bin);
+        $header = [];
+        while (strlen($bin = fread($this->file, 512)) !== 0) {
+            $this->headerBin[] = $bin;
+            $data = unpack("a100filename/a8mode/a8uid/a8gid/a12size/a12mtime/a8checksum/a1typeflag/a100link/a6magic/a2version/a32uname/a32gname/a8devmajor/a8devminor/a155prefix", $bin);
 
-			if($data['typeflag'] == Tar::TYPE_DIRECTORY) 
-				$data['size'] = 0;
+            if($data['typeflag'] == Tar::TYPE_DIRECTORY) 
+                $data['size'] = 0;
 
-			if($data['size'] == 0 && $data['typeflag'] != Tar::TYPE_DIRECTORY) 
-				continue;
+            if($data['size'] == 0 && $data['typeflag'] != Tar::TYPE_DIRECTORY) 
+                continue;
 
-			fseek($this->file, ftell($this->file) + 512 * ceil($data['size'] / 512));
-			$header[] = $data;
-		}
+            fseek($this->file, ftell($this->file) + 512 * ceil($data['size'] / 512));
+            $header[] = $data;
+        }
 
-		return $header;
+        return $header;
     }
 
     public function getFileNames() {
-    	$fileName = [];
-    	for ($i=0;$i<count($this->header);$i++) {
-    		$fileName[] = $this->header[$i]['filename'];
-    	}
-    	return $filename;
+        $fileName = [];
+        for ($i=0;$i<count($this->header);$i++) {
+            $fileName[] = $this->header[$i]['filename'];
+        }
+        return $filename;
     }
 }
 
