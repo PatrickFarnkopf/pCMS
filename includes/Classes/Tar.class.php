4991c44be3e47ff16822f585b2787062e950165c
Patrick Farnkopf <patrickfarnkopf@web.de>
2013-03-09 16:16:10 +0100

    Classes/Tar: Einzelne Dateien können nun entpackt werden

--- includes/Classes/Tar.class.php
+++ includes/Classes/Tar.class.php
@@ -20,8 +20,7 @@ class Tar {
         if (!file_exists($filename))
             throw new \Exception("Can not open file $filename. File does not exist!", 0x0A);
         
-        $this->file = fopen($filename, 'a+');
-        $this->content = fread($this->file, filesize($filename));
+        $this->file = fopen($filename, 'rb');
         $this->header = $this->readHeader();
     }
 
@@ -30,14 +29,19 @@ class Tar {
         while (strlen($bin = fread($this->file, 512)) !== 0) {
             $this->headerBin[] = $bin;
             $data = unpack("a100filename/a8mode/a8uid/a8gid/a12size/a12mtime/a8checksum/a1typeflag/a100link/a6magic/a2version/a32uname/a32gname/a8devmajor/a8devminor/a155prefix", $bin);
-
+            
+            $data['offset'] = ftell($this->file);
+            
             if($data['typeflag'] == Tar::TYPE_DIRECTORY) 
                 $data['size'] = 0;
+            else
+                $data['size'] = octdec($data['size']);
 
             if($data['size'] == 0 && $data['typeflag'] != Tar::TYPE_DIRECTORY) 
                 continue;
 
             fseek($this->file, ftell($this->file) + 512 * ceil($data['size'] / 512));
+            
             $header[] = $data;
         }
 
@@ -51,6 +55,19 @@ class Tar {
         }
         return $filename;
     }
+
+    public function extract($filename, $destination) {
+        for ($i=0;$i<count($this->header);$i++) {
+            if ($filename == $this->header[$i]['filename']) {
+                fseek($this->file, $this->header[$i]['offset']);
+                touch($destination.'/'.$this->header[$i]['filename']);
+                file_put_contents($destination.'/'.$this->header[$i]['filename'], fread($this->file, $this->header[$i]['size']));
+                return true;
+            }
+        }
+
+        return false;
+    } 
 }
 
 ?>
